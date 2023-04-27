22d6b9a12273d3e08fb84305a4d7e3133deb2cb1
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-27 07:36:52 +0200

    Model Synchronization

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const sequelize = new Sequelize('SEQUELIZE','root','',{
     dialect : 'mysql'
 })
 
-sequelize.authenticate().then(()=>{
-    console.log("connection successfully")
+const User = sequelize.define('user',{
+    username :{
+      type:Sequelize.DataTypes.STRING,
+      allowNull:false
+    },
+    password:{
+       type:Sequelize.DataTypes.STRING 
+    },
+    age:{
+       type:Sequelize.DataTypes.INTEGER,
+       defaultValue:21   
+    }
+});
+
+User.sync().then(()=>{
+console.log("table and model saved succcessfully")
 }).catch((err)=>{
-    console.log("error connecting to database")
+console.log("Error occured in synchronising"+err)
 })
+
+// sequelize.authenticate().then(()=>{
+//     console.log("connection successfully")
+// }).catch((err)=>{
+//     console.log("error connecting to db")
+// })
 
 
 
