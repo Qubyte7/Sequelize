af3a2f0264981a4e3f535a21554530320868f09d
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-27 08:35:41 +0200

    inserting a user into dtabase

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 const Sequelize = require('sequelize');
+const {DataTypes} = Sequelize
 const sequelize = new Sequelize('SEQUELIZE','root','',{
-    dialect : 'mysql'
+    dialect : 'mysql',//here we are spcfind the db
+    define:{
+    freezeTableName:true//this is done when you want this to happen on each table
+    }
 })
+ //sequelize.sync({force:true}) -->synchronous every tablein that database individually ath the same time
 
 const User = sequelize.define('user',{
+    user_id:{
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,  
+  },
     username :{
-      type:Sequelize.DataTypes.STRING,
+      type:DataTypes.STRING,
       allowNull:false
-    },
+  },
     password:{
-       type:Sequelize.DataTypes.STRING 
-    },
+       type:DataTypes.STRING 
+  },
     age:{
-       type:Sequelize.DataTypes.INTEGER,
+       type:DataTypes.INTEGER,
        defaultValue:21   
-    }
+  },
+    wittCodeRcoks:{
+       type:DataTypes.BOOLEAN,
+       defaultValue:true,
+  }
+},{
+  freezeTableName:true,//help to retain the name of table in database to be same as one of the model
+  timestamps:false,//remove the createdAt && UpdateAt time stamps that are automatically created sequelize when we create a table
 });
 
-User.sync().then(()=>{
-console.log("table and model saved succcessfully")
-}).catch((err)=>{
-console.log("Error occured in synchronising"+err)
-})
+
+//synchronising table
+User.sync({alter:true}).then(()=>{
+const userCreated = User.build({username:'innocent',password:'123456',age:16,wittCodeRcoks:false})
+return userCreated.save();//this saves the information in our databse
+}).then((Data)=>{console.log("Data inserted")})
+.catch((err)=>{console.log("Error occured in synchronising"+err)})
+ 
+
+
+ //for dropping a table
+          // User.drop();
+          //sequelize.drop();->dropping every table in that database at the same time
+           //sequelize.drop({match: /_test$/ });//here we specified the table that would be dropped that would be ending by '_test'
 
 // sequelize.authenticate().then(()=>{
 //     console.log("connection successfully")
