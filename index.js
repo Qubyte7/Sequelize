23629c4687b65e948ed6d54dea55d7c33d6d56bd
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-28 05:36:34 +0200

    appllaying function logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const {DataTypes} = Sequelize
+const {DataTypes,Op} = Sequelize
 const sequelize = new Sequelize('SEQUELIZE','root','',{
     dialect : 'mysql',//here we are spcfind the db
     define:{
@@ -16,7 +16,10 @@ const User = sequelize.define('user',{
   },
     username :{
       type:DataTypes.STRING,
-      allowNull:false
+      allowNull:false,
+      validate:{
+        len :[2,20]
+      }
   },
     password:{
        type:DataTypes.STRING 
@@ -37,9 +40,56 @@ const User = sequelize.define('user',{
 
 //synchronising table
 User.sync({alter:true}).then(()=>{
-const userCreated = User.build({username:'innocent',password:'123456',age:16,wittCodeRcoks:false})
-return userCreated.save();//this saves the information in our databse
-}).then((Data)=>{console.log("Data inserted")})
+           //first way
+// const userCreated = User.build({username:'innocent',password:'123456',age:16,wittCodeRcoks:false})
+// return userCreated.save();
+             //Second way
+// return  User.create({username:'Ganza',password:'123456',age:34,wittCodeRcoks:false})
+            //Multiple Input
+// return User.bulkCreate([
+//   {username:'Ganza',password:'123456',age:34,wittCodeRcoks:false},
+//   {username:'Sabrina',password:'654321',age:43,wittCodeRcoks:true}
+// ],{validate:true})//for accepting validation
+
+
+return User.findAll({attributes:['username','password']});//Here i want these specified field to display
+return User.findAll({attributes:[['username','name'],['password','pwd']]});//Here display username as name and passwrd as pwd
+return User.findAll({attributes:[[sequelize.fn('SUM',sequelize.col('age')),'howOld']]});//here we are finding the sum of the age col values
+return User.findAll({attributes:{exclude : ['password']}});//excluding a field
+return User.findAll({attributes:['username'],where:{age:45}});
+return User.findAll({attributes:{where:{age:45,username:'ganza'}}});
+return User.findAll({limit:2});//return 2 rows
+return User.findAll({order:[['age','DESC']]});//ordering age in descending order
+return User.findAll({attributes:['username',[sequelize.fn('SUM',sequelize.col('age')),'sum_age']],group : 'username'});//rertun the name in dataBase with summing up tha ages of people with the same name
+return User.findAll({where:{[Op.or]:{username:'ganza',age:45}}});//find where the username = ganza OR where age = 45
+return User.findAll({where:{age:{[Op.gt]:25}}});//where age is greater than 25
+
+
+
+}).then((data)=>{
+  // console.log("User created inserted")
+  // console.log(data.toJSON())
+  // data.username = 'pizza';
+  // data.age = 23;
+  // data.increment({age:2});
+//return data.save({fields:['age']});//here age is the only one to be saved or changed
+//return data.destroy();
+//return data.reload();
+         //How to display bulk Users
+// data.forEach((element) => {
+//     console.log(element.toJSON());
+// });
+data.forEach((element)=>{
+  console.log(element.toJSON())
+})
+
+
+})
+// .then((data)=>{
+//console.log("User returned to normal")
+//console.log("User deleted")
+// console.log("user Updated")
+// console.log(data.toJSON())})
 .catch((err)=>{console.log("Error occured in synchronising"+err)})
  
 
